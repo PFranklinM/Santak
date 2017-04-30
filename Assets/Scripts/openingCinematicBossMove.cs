212da80b54653a745a44b9e9806ef5e4b0e5fa5f
PFranklinM <paulmallon@Pauls-MacBook-Pro.local>
2017-04-30 00:04:02 -0400

    ProgressOnOpeningCinematic
    here

---
+++ Assets/Scripts/openingCinematicBossMove.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class openingCinematicBossMove : MonoBehaviour {
+
+	float moveSpeed;
+
+	public bool timeToMoveRight;
+
+	// Use this for initialization
+	void Start () {
+
+		moveSpeed = 85;
+
+		timeToMoveRight = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 bossMove = new Vector3 (transform.position.x,
+			                   transform.position.y,
+			                   transform.position.z);
+
+		if (bossMove.y <= -90) {
+			bossMove.y += moveSpeed * Time.deltaTime;
+		}
+
+		if (bossMove.y >= -90) {
+			timeToMoveRight = true;
+		}
+
+		if (timeToMoveRight == true) {
+			bossMove.x += moveSpeed * Time.deltaTime;
+		}
+
+		transform.position = bossMove;
+	
+	}
+}
---
+++ Assets/Scripts/openingCinematicCameraFollow.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class openingCinematicCameraFollow : MonoBehaviour {
+
+	public GameObject boss;
+
+	float offset;
+
+	// Use this for initialization
+	void Start () {
+
+		Vector3 bossPos = new Vector3 (boss.transform.position.x,
+			boss.transform.position.y,
+			boss.transform.position.z);
+
+		Vector3 cameraPos = new Vector3 (transform.position.x,
+			transform.position.y,
+			transform.position.z);
+
+		offset = cameraPos.x - bossPos.x;
+
+		transform.position = cameraPos;
+
+		boss.transform.position = bossPos;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LateUpdate () {
+
+		if (GameObject.Find ("FinalBoss").GetComponent<openingCinematicBossMove> ().timeToMoveRight == true) {
+
+			Vector3 bossPos = new Vector3 (boss.transform.position.x,
+				boss.transform.position.y,
+				boss.transform.position.z);
+
+			Vector3 cameraPos = new Vector3 (transform.position.x,
+				transform.position.y,
+				transform.position.z);
+
+			cameraPos.x = bossPos.x + offset;
+
+			transform.position = cameraPos;
+
+			boss.transform.position = bossPos;
+		}
+
+	}
+}
