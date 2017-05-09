11275b81d8ccf089c8a7f6903338bb1be0043c2e
PFranklinM <paulmallon@Pauls-MBP.wireless.newschool.edu>
2017-05-09 16:33:04 -0400

    savingIt
    yay

---
+++ Assets/Scripts/activateInstructions6.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class activateInstructions6 : MonoBehaviour {
+
+	public TextAsset theText;
+
+	public int startLine;
+	public int endLine;
+
+	public playerTextBoxManager textBox;
+
+	public bool textHasPlayed = false;
+
+	// Use this for initialization
+	void Start () {
+
+		textBox = FindObjectOfType<playerTextBoxManager> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (GameObject.Find ("Player").GetComponent<playerMove> ().thirdPlaythrough == true) {
+			textHasPlayed = false;
+		}
+
+		if (GameObject.Find ("Player").GetComponent<playerMove> ().thirdPlaythrough == false) {
+			textHasPlayed = true;
+		}
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D coll){
+		if (coll.gameObject.tag == "player" && textHasPlayed == false &&
+			GameObject.Find ("Player").GetComponent<playerMove> ().thirdPlaythrough == true) {
+
+			textBox.reloadScript (theText);
+			textBox.currentLine = startLine;
+			textBox.endAtLine = endLine;
+			textBox.enableTextBox ();
+		}
+	}
+}
---
+++ Assets/Scripts/activateInstructions7.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class activateInstructions7 : MonoBehaviour {
+
+	public TextAsset theText;
+
+	public int startLine;
+	public int endLine;
+
+	public playerTextBoxManager textBox;
+
+	public bool textHasPlayed = false;
+
+	public GameObject X;
+
+	// Use this for initialization
+	void Start () {
+
+		textBox = FindObjectOfType<playerTextBoxManager> ();
+
+		X.SetActive (false);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D coll){
+		if (coll.gameObject.tag == "player" && textHasPlayed == false) {
+
+			textBox.reloadScript (theText);
+			textBox.currentLine = startLine;
+			textBox.endAtLine = endLine;
+			textBox.enableTextBox ();
+
+			X.SetActive (true);
+
+			GameObject.Find ("Target").GetComponent<targetControl> ().AREquipped = false;
+			GameObject.Find ("Target").GetComponent<targetControl> ().MLEquipped = true;
+
+			textHasPlayed = true;
+		}
+	}
+}
